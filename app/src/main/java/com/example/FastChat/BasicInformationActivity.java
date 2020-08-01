0963ed5d46b5d7eea90f785bd0c7357d67db146d
Ragnar <42907075+ragnar-ji@users.noreply.github.com>
2020-08-01 20:50:34 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/FastChat/BasicInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.FastChat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_information);
+    }
+}
---
+++ app/src/main/java/com/example/FastChat/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.FastChat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button logOutButton;
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        logOutButton = findViewById(R.id.btn_logout);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (currentUser==null){
+            Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+    }
+}
